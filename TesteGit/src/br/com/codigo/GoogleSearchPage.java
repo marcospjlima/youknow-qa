d056a21cbb7d2b733cc956d04bd24a8bd212cdc0
Marcos Paulo de Jesus <marcospjlima@yahoo.com.br>
2013-01-08 11:18:53 -0200

    2

---
+++ TesteGit/src/br/com/codigo/GoogleSearchPage.java
@@ -0,0 +1,15 @@
+package br.com.codigo;
+
+import org.openqa.selenium.WebElement;
+
+public class GoogleSearchPage {
+    // Here's the element
+    private WebElement q;
+
+    public void searchFor(String text) {
+        // And here we use it. Note that it looks like we've
+        // not properly instantiated it yet....
+        q.sendKeys(text);
+        q.submit();
+    }
+}
